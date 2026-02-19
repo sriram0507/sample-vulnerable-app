8d1777950777dc06c4f90cf534fa700b5bc8a780
sriram0507 <85109192+sriram0507@users.noreply.github.com>
2026-02-19 12:56:05 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,35 +14,47 @@ provider "aws" {
 
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "sample-app-terraform-bucket-12345"
-  acl    = "public-read"                        # Issue 1: public-read ACL
+  acl    = "private"  # Changed from "public-read" to "private" for better security
 }
 
 resource "aws_iam_policy" "app_policy" {
-  name        = "app-full-access"
-  description = "Policy used by instances"
+  name        = "app-restricted-access"  # Changed name to reflect restricted access
+  description = "Policy used by instances with least privilege principle"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "*",                             # Issue 2: wildcard actions
-      "Resource": "*"                            # Issue 3: wildcard resources
-    }
-  ]
-}
-EOF
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::sample-app-terraform-bucket-12345",
+          "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
+        ]
+      }
+    ]
+  })
 }
 
-resource "aws_security_group" "open_sg" {
-  name        = "open-sg"
-  description = "Security group with wide open access"
+resource "aws_security_group" "restricted_sg" {
+  name        = "restricted-sg"
+  description = "Security group with restricted access"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]                 # Issue 4: all ports open to the world
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]  # Restrict to internal network
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]  # Restrict to internal network
   }
 }
